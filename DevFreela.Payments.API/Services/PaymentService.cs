2bddd2e50faaea03d5bb2125ea8eaac08bbdf4d0
fillipe.felix <felipesoares_1993@hotmail.com>
2021-12-27 23:04:15 -0300

    criando o endpoint para processamento de pagamentos para a api de pagamentos

---
+++ DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using DevFreela.Payments.API.Models;
+
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel)
+        {
+            // Processa o pagamento
+
+            return Task.FromResult(true);
+        }
+    }
+}
